83f6e38d0c76899e4b6cc16ebf5ba3228320b2c8
DenkovychOleh <praice07@gmail.com>
2023-09-17 17:13:26 +0300

    refactoring

--- dispatcher/src/main/java/com/dnk/configuration/RabbitConfiguration.java
+++ dispatcher/src/main/java/com/dnk/configuration/RabbitConfiguration.java
@@ -1,11 +1,13 @@
 package com.dnk.configuration;
 
-import com.dnk.model.RabbitQueue;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static com.dnk.model.RabbitQueue.ANSWER_MESSAGE;
+import static com.dnk.model.RabbitQueue.TEXT_MESSAGE_UPDATED;
 
 @Configuration
 public class RabbitConfiguration {
@@ -15,10 +17,10 @@ public class RabbitConfiguration {
     }
     @Bean
     public Queue textMessageQueue() {
-        return new Queue(RabbitQueue.TEXT_MESSAGE_UPDATED);
+        return new Queue(TEXT_MESSAGE_UPDATED);
     }
     @Bean
     public Queue answerMessageQueue() {
-        return new Queue(RabbitQueue.ANSWER_MESSAGE);
+        return new Queue(ANSWER_MESSAGE);
     }
 }
