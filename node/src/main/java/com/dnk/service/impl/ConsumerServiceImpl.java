035a35a53eaf66bc821b2d80703b381a53da5d26
DenkovychOleh <praice07@gmail.com>
2023-09-17 18:02:21 +0300

    Added RawData entity and dao, included postgresql

--- node/src/main/java/com/dnk/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/com/dnk/service/impl/ConsumerServiceImpl.java
@@ -1,12 +1,10 @@
 package com.dnk.service.impl;
 
 import com.dnk.service.ConsumerService;
-import com.dnk.service.ProducerService;
+import com.dnk.service.MainService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static com.dnk.model.RabbitQueue.TEXT_MESSAGE_UPDATED;
@@ -14,21 +12,16 @@ import static com.dnk.model.RabbitQueue.TEXT_MESSAGE_UPDATED;
 @Log4j
 @Service
 public class ConsumerServiceImpl implements ConsumerService {
-    private final ProducerService producerService;
+    private final MainService mainService;
 
-    public ConsumerServiceImpl(ProducerService producerService) {
-        this.producerService = producerService;
+    public ConsumerServiceImpl(MainService mainService) {
+        this.mainService = mainService;
     }
 
     @Override
     @RabbitListener(queues = TEXT_MESSAGE_UPDATED)
     public void consumeTextMessageUpdates(Update update) {
         log.debug("NODE : Text message is received");
-
-        Message message = update.getMessage();
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from NODE");
-        producerService.produceAnswer(sendMessage);
+        mainService.processTextMessage(update);
     }
 }
---
+++ node/src/main/java/com/dnk/service/impl/MainServiceImpl.java
@@ -0,0 +1,40 @@
+package com.dnk.service.impl;
+
+import com.dnk.dao.RawDataDAO;
+import com.dnk.entity.RawData;
+import com.dnk.service.MainService;
+import com.dnk.service.ProducerService;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class MainServiceImpl implements MainService {
+    private final RawDataDAO rawDataDAO;
+
+    private final ProducerService producerService;
+
+    public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService) {
+        this.rawDataDAO = rawDataDAO;
+        this.producerService = producerService;
+    }
+
+    @Override
+    public void processTextMessage(Update update) {
+        saveRawData(update);
+
+        Message message = update.getMessage();
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(message.getChat().getFirstName() + ", hello from NODE");
+        producerService.produceAnswer(sendMessage);
+    }
+
+    private void saveRawData(Update update) {
+        RawData rawData = RawData.builder()
+                .event(update)
+                .build();
+        rawDataDAO.save(rawData);
+    }
+}
